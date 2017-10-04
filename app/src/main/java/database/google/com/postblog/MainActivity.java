5c6da15edb12bb75d267ff0b718471611f00da48
Your Name <you@example.com>
2017-10-04 09:02:34 +0600

    Added PostActivity.java.Here the recyclerCard view is listed and in it Image and Textciew also added

--- app/src/main/java/database/google/com/postblog/MainActivity.java
+++ app/src/main/java/database/google/com/postblog/MainActivity.java
@@ -1,10 +1,13 @@
 package database.google.com.postblog;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -82,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
                 if(task.isSuccessful()){
 
                     Toast.makeText(MainActivity.this,"You are signedI",Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(MainActivity.this,PostListActivity.class);
+                    startActivity(intent);
                 }
 
                 else{
@@ -93,8 +98,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId()==R.id.action_signout){
+
+            mAuth.signOut();
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener!=null){
+mAuth.removeAuthStateListener(mAuthListener);
+
+        }
     }
 }
---
+++ app/src/main/java/database/google/com/postblog/PostListActivity.java
@@ -0,0 +1,13 @@
+package database.google.com.postblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_list);
+    }
+}
